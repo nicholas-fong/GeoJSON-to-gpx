65dc4fe75dedbad6d9bb4754fa730c741696f5ec
Nicholas Fong <nicholas.fong@gmail.com>
2023-10-07 20:57:55 -0700

    Update geo2geo-compact.py

--- library/geo2geo-compact.py
+++ library/geo2geo-compact.py
@@ -1,18 +1,16 @@
-# compact GeoJSON file into one line. Machine readable, more compact, but less human readable
-
 import sys
 import geojson
-from geojson import FeatureCollection, Feature
+from geojson import FeatureCollection, Feature, Point, LineString, Polygon, dumps
 
 if len(sys.argv) < 2:
-    print("enter a geojson file to compact ")
+    print("enter a geojson file to convert to geojson file ")
     sys.exit(1)
 
-basket = []
-      
 with open( sys.argv[1]+'.geojson', 'r') as infile:
    data = geojson.load ( infile )
 infile.close()   
+
+basket = []       
 
 for i in range(len(data['features'])):
     my_type = data['features'][i]['geometry']['type']
@@ -23,9 +21,17 @@ for i in range(len(data['features'])):
         my_feature = Feature(geometry=my_geometry, properties=my_properties)
         basket.append(my_feature)
 
+    elif my_type == 'MultiPoint':
+        my_feature = Feature(geometry=my_geometry, properties=my_properties)
+        basket.append(my_feature)        
+
     elif my_type == 'LineString':
         my_feature = Feature(geometry=my_geometry, properties=my_properties)
-        basket.append(my_feature)   
+        basket.append(my_feature)
+
+    elif my_type == 'MultiLineString':
+        my_feature = Feature(geometry=my_geometry, properties=my_properties)
+        basket.append(my_feature)
 
     elif my_type == 'Polygon':
         my_feature = Feature(geometry=my_geometry, properties=my_properties)
@@ -33,10 +39,10 @@ for i in range(len(data['features'])):
 
     elif my_type == 'MultiPolygon':
         my_feature = Feature(geometry=my_geometry, properties=my_properties)
-        basket.append(my_feature)          
+        basket.append(my_feature)   
 
-out_str = str(FeatureCollection(basket)).replace("Name","name").replace("NAME","name")
+geojson_string = dumps(FeatureCollection(basket))
+cleaner_str = geojson_string.replace("Name","name").replace("NAME","name")
 
 with open( sys.argv[1]+'.geojson', 'w') as outfile:
-    outfile.write( out_str )
-
+    outfile.write( cleaner_str )
