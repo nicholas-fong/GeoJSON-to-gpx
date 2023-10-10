1824b975b8b23b6b17e7d1df695b6bda478d89d9
Nicholas Fong <nicholas.fong@gmail.com>
2023-10-09 22:22:02 -0700

    update geojson to gpx conversions

--- library/geo2rte.py
+++ library/geo2rte.py
@@ -1,46 +1,31 @@
-# GeoJSON
-# convert LineStrings to routes
-# Polygons and Points are discarded
-
+# convert GeoJSON LineStrings to gpx routes  # Polygons and Points are discarded
 import sys
 import geojson
 import gpxpy
-import gpxpy.gpx
 
 with open( sys.argv[1]+'.geojson', 'r') as infile:
    data = geojson.load ( infile )
-infile.close()
 
 new = gpxpy.gpx.GPX()   #create a new gpx object
 
-for i in range(len(data['features'])):
-    geom = data['features'][i]['geometry']
-    node = geom['coordinates']
-    
-    if ( geom['type'] == 'LineString' ):
-        try:
-            myname = data['features'][i]['properties']['name']
-        except:
-            try:
-                myname = data['features'][i]['properties']['Name']
-            except:
-                try:
-                    myname = data['features'][i]['properties']['NAME']
-                except:    
-                    myname = 'noname'
-
-        new_route =  gpxpy.gpx.GPXRoute( myname )       
-        new.routes.append(new_route)
-
-        for j in range(len(node)):
-            if (len(node[j])) == 2:
-                new_route.points.append(gpxpy.gpx.GPXRoutePoint(node[j][1],node[j][0]))
-            else:
-                new_route.points.append(gpxpy.gpx.GPXRoutePoint(node[j][1],node[j][0],node[j][2]))   
+if 'features' in data:
+    features = data['features']
+    for feature in features:
+        geometry_type = feature['geometry'].get('type', None)
+        coordinates = feature['geometry'].get('coordinates', None)
+        properties = feature.get('properties', None)
+            
+        if geometry_type == 'LineString':
+            route_name =  properties.get('name') or properties.get('Name') or properties.get('NAME')
+            new_route =  gpxpy.gpx.GPXRoute( route_name )        
+            new.routes.append(new_route)
+            for item in coordinates:
+                if len(item)==2:
+                    new_route.points.append(gpxpy.gpx.GPXRoutePoint(item[1],item[0]))
+                else:    
+                    new_route.points.append(gpxpy.gpx.GPXRoutePoint(item[1],item[0],item[2]))
 
 print( new.to_xml() )
 
 with open(sys.argv[1]+'.gpx', 'w') as file:
     file.write( new.to_xml() )
-
-
--- library/geo2wpt.py
+++ library/geo2wpt.py
@@ -1,64 +1,32 @@
-# GeoJSON
-# convert Points to waypoints
-# convert polygons to waypoints based on polygon's centroid
-# LineStrings are discarded
-
+# convert GeoJSON Points to gpx waypoints  # LineString and Polygons are discarded
 import sys
-from statistics import mean
-import geojson
 import gpxpy
-import gpxpy.gpx
+import geojson
 
-with open( sys.argv[1]+'.geojson', 'r') as infile:
-   data = geojson.load ( infile )
-infile.close()
+with open(sys.argv[1]+'.geojson', 'r') as file:
+    data = geojson.load(file)
 
 new = gpxpy.gpx.GPX()   #create a gpx object
 
-for i in range(len(data['features'])):
-    geom = data['features'][i]['geometry']
-    node = geom['coordinates']
+if 'features' in data:
+    features = data['features']
     
-    if ( geom['type'] == 'Point' ):         # Point, simple lat lon output
-        new_wpt = gpxpy.gpx.GPXWaypoint()    # create new point object
-        try:
-            myname = data['features'][i]['properties']['name']
-        except:
-            try:
-                myname = data['features'][i]['properties']['Name']
-            except:
-                myname = 'noname'
-        new_wpt.name = myname
-        if (len(node)) == 2:
-            new_wpt.latitude = node[1]
-            new_wpt.longitude = node[0]
-        else:    
-            new_wpt.latitude = node[1]
-            new_wpt.longitude = node[0]
-            new_wpt.elevation = node[2]    
-        new.waypoints.append(new_wpt)
+    for feature in features:
+        geometry_type = feature['geometry'].get('type', None)
+        coordinates = feature['geometry'].get('coordinates', None)
+        properties = feature.get('properties', None)
 
-    if ( geom['type'] == 'Polygon' ):     # if Polygon, calculate centroid and consider it as a Point
-        new_wpt = gpxpy.gpx.GPXWaypoint()
-        pnode = data['features'][i]['geometry']['coordinates'][0]
-        bucket1=[]
-        bucket2=[]
-        for j in range(len(pnode)-1):
-            bucket1.append( pnode[j][1] )
-            bucket2.append( pnode[j][0] )
-        try:
-            myname = data['features'][i]['properties']['name']
-        except:
-            try:
-                myname = data['features'][i]['properties']['Name']
-            except:
-                myname = 'noname'
-        new_wpt.name = myname
-        new_wpt.latitude = mean(bucket1)
-        new_wpt.longitude = mean(bucket2)
-        new.waypoints.append(new_wpt)
-
+        if geometry_type == 'Point':
+            new_wpt = gpxpy.gpx.GPXWaypoint()    # create new point object
+            new_wpt.name = properties.get('name') or properties.get('Name') or properties.get('NAME')
+            new_wpt.latitude = coordinates[1]
+            new_wpt.longitude = coordinates[0]
+            if (len(coordinates)) > 2:
+                new_wpt.elevation = coordinates[2]    
+            new.waypoints.append(new_wpt)
+            
 print( new.to_xml() )
 
 with open(sys.argv[1]+'.gpx', 'w') as file:
     file.write( new.to_xml() )
+    
