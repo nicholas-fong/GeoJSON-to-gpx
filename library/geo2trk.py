fdd75b8b2af7d491aa57f757f0589a7a7b118fec
Nicholas Fong <nicholas.fong@gmail.com>
2023-10-10 08:21:21 -0700

    update geo2trk and geo2rte

--- library/geo2trk.py
+++ library/geo2trk.py
@@ -8,13 +8,25 @@ with open( sys.argv[1]+'.geojson', 'r') as infile:
 
 new = gpxpy.gpx.GPX()    #create a new gpx object
 
+answer = input("Do you want to include waypoints also ? Y or N ")
+
 if 'features' in data:
     features = data['features']
     for feature in features:
         geometry_type = feature['geometry'].get('type', None)
         coordinates = feature['geometry'].get('coordinates', None)
         properties = feature.get('properties', None)
-            
+
+        if answer == 'Y' or answer == 'y':
+            if geometry_type == 'Point':
+                new_wpt = gpxpy.gpx.GPXWaypoint()    # create new point object
+                new_wpt.name = properties.get('name') or properties.get('Name') or properties.get('NAME')
+                new_wpt.latitude = coordinates[1]
+                new_wpt.longitude = coordinates[0]
+                if (len(coordinates)) > 2:
+                    new_wpt.elevation = coordinates[2]    
+                new.waypoints.append(new_wpt)
+
         if geometry_type == 'LineString':
             track_name =  properties.get('name') or properties.get('Name') or properties.get('NAME')
             new_track =  gpxpy.gpx.GPXTrack( track_name )        
