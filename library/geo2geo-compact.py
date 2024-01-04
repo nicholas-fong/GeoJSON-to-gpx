230fe3b4c9bccd10fc4522d7333ac226af0c2b4c
Nicholas Fong <nicholas.fong@gmail.com>
2024-01-04 14:46:20 -0800

    Update geo2geo-compact.py

--- library/geo2geo-compact.py
+++ library/geo2geo-compact.py
@@ -1,19 +1,19 @@
 import sys
 import re
-import geojson
-from geojson import FeatureCollection, Feature, dumps
+import json
+from geojson import FeatureCollection, Feature
 
 # GeoJSON into one line
 
 with open( sys.argv[1]+'.geojson', 'r') as infile:
-   data = geojson.load ( infile )
+   data = json.load ( infile )
 infile.close()   
 
 basket = []       
 for feature in data['features']:
     basket.append(Feature(geometry=feature['geometry'], properties=feature['properties']))
 
-geojson_string = dumps(FeatureCollection(basket), ensure_ascii=False)
+geojson_string = json.dumps(FeatureCollection(basket), ensure_ascii=False)
 # clean up some programs that use different spellings for name
 output_string = re.sub(r'name', 'name', geojson_string, flags=re.IGNORECASE)
 
