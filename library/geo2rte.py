2f916340b111fcb55b208c5fa884cebd063b604d
Nicholas Fong <nicholas.fong@gmail.com>
2024-01-04 14:44:15 -0800

    Update geo2rte.py

--- library/geo2rte.py
+++ library/geo2rte.py
@@ -1,12 +1,12 @@
 # convert GeoJSON LineStrings to gpx routes  # Polygons and Points are discarded
 import sys
-import geojson
+import json
 import gpxpy
 
 with open( sys.argv[1]+'.geojson', 'r') as infile:
-   data = geojson.load ( infile )
+   data = json.load ( infile )
 
-new = gpxpy.gpx.GPX()   #create a new gpx object
+gpx = gpxpy.gpx.GPX()   #create a new gpx object
 
 answer = input("Do you want to include waypoints also ? Y or N ")
 
@@ -25,19 +25,19 @@ if 'features' in data:
                 new_wpt.longitude = coordinates[0]
                 if (len(coordinates)) > 2:
                     new_wpt.elevation = coordinates[2]    
-                new.waypoints.append(new_wpt)
+                gpx.waypoints.append(new_wpt)
 
         if geometry_type == 'LineString':
             route_name =  properties.get('name') or properties.get('Name') or properties.get('NAME')
             new_route =  gpxpy.gpx.GPXRoute( route_name )        
-            new.routes.append(new_route)
+            gpx.routes.append(new_route)
             for item in coordinates:
                 if len(item)==2:
                     new_route.points.append(gpxpy.gpx.GPXRoutePoint(item[1],item[0]))
                 else:    
                     new_route.points.append(gpxpy.gpx.GPXRoutePoint(item[1],item[0],item[2]))
 
-print( new.to_xml() )
+#print( gpx.to_xml() )
 
 with open(sys.argv[1]+'.gpx', 'w') as file:
-    file.write( new.to_xml() )
+    file.write( gpx.to_xml() )
