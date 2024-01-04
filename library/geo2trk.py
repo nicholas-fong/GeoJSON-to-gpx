5ef29cccb9c7c87cf4725718463ffc9caab90e67
Nicholas Fong <nicholas.fong@gmail.com>
2024-01-04 14:43:46 -0800

    Update geo2trk.py

--- library/geo2trk.py
+++ library/geo2trk.py
@@ -1,12 +1,12 @@
 # convert GeoJSON LineStrings to gpx tracks  # Points and Polygons are discarded
 import sys
-import geojson
+import json
 import gpxpy
 
 with open( sys.argv[1]+'.geojson', 'r') as infile:
-   data = geojson.load ( infile )
+   data = json.load ( infile )
 
-new = gpxpy.gpx.GPX()    #create a new gpx object
+gpx = gpxpy.gpx.GPX()    #create a new gpx object
 
 answer = input("Do you want to include waypoints also ? Y or N ")
 
@@ -25,12 +25,12 @@ if 'features' in data:
                 new_wpt.longitude = coordinates[0]
                 if (len(coordinates)) > 2:
                     new_wpt.elevation = coordinates[2]    
-                new.waypoints.append(new_wpt)
+                gpx.waypoints.append(new_wpt)
 
         if geometry_type == 'LineString':
             track_name =  properties.get('name') or properties.get('Name') or properties.get('NAME')
             new_track =  gpxpy.gpx.GPXTrack( track_name )        
-            new.tracks.append( new_track )
+            gpx.tracks.append( new_track )
             new_segment = gpxpy.gpx.GPXTrackSegment()
             new_track.segments.append(new_segment)
             for item in coordinates:
@@ -39,8 +39,8 @@ if 'features' in data:
                 else:    
                     new_segment.points.append(gpxpy.gpx.GPXTrackPoint(item[1],item[0],item[2]))
 
-print( new.to_xml() )
+#print( gpx.to_xml() )
 
 with open(sys.argv[1]+'.gpx', 'w') as file:
-    file.write( new.to_xml() )
+    file.write( gpx.to_xml() )
     
