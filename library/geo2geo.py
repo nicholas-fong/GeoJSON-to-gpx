c353ce3ab6e239a5b14f7f2c616084a8b8e0a5af
Nicholas Fong <nicholas.fong@gmail.com>
2024-07-14 15:09:38 -0700

    Update geo2geo.py
    add error checking

--- library/geo2geo.py
+++ library/geo2geo.py
@@ -4,7 +4,6 @@ import re
 from geojson import FeatureCollection, Feature
 
 # remove newlines and blanks in the coordinates array, for better readibility of the GeoJSON pretty print
-
 def custom_dumps(obj, **kwargs):
     def compact_coordinates(match):
         # Remove newlines and extra spaces within the coordinates array
@@ -15,8 +14,15 @@ def custom_dumps(obj, **kwargs):
     json_str = re.sub(r'\[\s*([^\[\]]+?)\s*\]', compact_coordinates, json_str)
     return json_str
 
-with open(sys.argv[1] + '.geojson', 'r', encoding='utf-8') as infile:
+try:
+    with open(sys.argv[1] + '.geojson', 'r', encoding='utf-8') as infile:
     data = json.load(infile)
+except FileNotFoundError:
+    print("file not found")
+    sys.exit(1)
+except json.JSONDecodeError:
+    print("Error: Failed to parse GeoJSON file.")
+    sys.exit(1)        
 
 bucket = []
 for feature in data['features']:
